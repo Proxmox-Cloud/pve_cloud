922ee53ecffd8a666b99373646309ea8b981edd9
tobi <tobi@tobias-huebner.tech>
2026-02-27 09:32:44 +0000

    Update pve-cloud-schemas version to 3.0.3

--- mkdocs-gen.sh
+++ mkdocs-gen.sh
@@ -5,7 +5,7 @@ set -e
 # it uses the pve-cloud-schemas package to validate our samples and dump / generate schema files in markdown format
 # for the documentation
 
-pip install pve-cloud-schemas==3.0.2
+pip install pve-cloud-schemas==3.0.3
 
 # validate samples
 cd samples
